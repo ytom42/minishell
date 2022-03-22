8e1196883baa34428bdc5209e657bbea6f8812fd
Yutaro Tomiyoshi <ytomiyos@c1r19s11.42tokyo.jp>
2022-03-22 16:18:06 +0900

    不要なコード削除

--- src/execute/exec_cmd.c
+++ src/execute/exec_cmd.c
@@ -6,7 +6,7 @@
 /*   By: ytomiyos <ytomiyos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 16:11:56 by kfumiya           #+#    #+#             */
-/*   Updated: 2022/03/21 18:36:15 by kfumiya          ###   ########.fr       */
+/*   Updated: 2022/03/22 15:54:24 by ytomiyos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,6 @@ void
 	path = create_executable_path(args[0]);
 	if (execve(path, args, envs) < 0)
 		exit_execve_error(path);
-	instant_free(envs);
-	free(path);
 }
 
 static void
